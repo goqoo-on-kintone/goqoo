7350bb9bdcbf06a5f6c207fd757be3fb8b145bc0
Hisaki Akaza <TheRed@users.noreply.github.com>
2018-05-24 19:47:20 +0900

    feat: generateコマンド ヘルプ出力だけ作成

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 const { createOptionValues, usageExit } = require('./lib/goqoo')
 const { goqooNew } = require('./lib/goqoo-new')
+const { goqooGenerate } = require('./lib/goqoo-generate')
 
 const main = async () => {
   const opts = await createOptionValues()
@@ -10,6 +11,9 @@ const main = async () => {
     switch (opts.type) {
       case 'new':
         await goqooNew(opts)
+        break
+      case 'generate':
+        await goqooGenerate(opts)
         break
       default:
         usageExit(1)
