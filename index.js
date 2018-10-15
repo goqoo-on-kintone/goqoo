76444438384092318c44dab7b4be404abc7ff608
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2018-10-16 02:40:04 +0900

    feat: newとgenerateはyeomanを間接的に呼び出す仕様に変更！

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
 #!/usr/bin/env node
 'use strict'
 
-const { createOptionValues, usageExit } = require('./lib/goqoo')
-const { goqooNew } = require('./lib/goqoo-new')
-const { goqooGenerate } = require('./lib/goqoo-generate')
+const { parseArgumentOptions, showVersion, usageExit } = require('./lib/goqoo')
+const { spawnSync } = require('child_process')
+const path = require('path')
+const fs = require('fs-extra')
 
-const main = async () => {
-  const opts = await createOptionValues()
-  try {
-    switch (opts.type) {
-      case 'new':
-        await goqooNew(opts)
-        break
-      case 'generate':
-        await goqooGenerate(opts)
-        break
-      default:
-        usageExit(1)
-    }
-  } catch (error) {
-    console.error(error)
+const argv = parseArgumentOptions()
+if (!argv.subCommand) {
+  if (argv.version) {
+    showVersion()
+  } else if (argv.help) {
+    usageExit(0)
+  } else {
+    usageExit(1)
   }
 }
 
-main()
+const rawArgv = process.argv.slice(3)
+let subGenerator
+let cwd = null
+
+// TODO: --helpの扱いをうまくやる
+switch (argv.subCommand) {
+  case 'init':
+    subGenerator = 'app'
+    break
+  case 'new':
+    subGenerator = 'app'
+    const projectDir = rawArgv.shift()
+    cwd = path.resolve(projectDir)
+    fs.mkdirpSync(cwd)
+    break
+  case 'generate':
+  case 'g':
+    subGenerator = `g-${rawArgv.shift()}`
+    break
+  default:
+    usageExit(1)
+}
+
+const yeoman = spawnSync('yo', [`goqoo:${subGenerator}`, ...rawArgv], { stdio: 'inherit', cwd })
+process.exit(yeoman.status)
