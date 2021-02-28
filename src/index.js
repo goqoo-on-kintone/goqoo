edf280e3b0b0a5f313a872540f92eb96d23ec880
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-02-28 21:30:34 +0900

    Revert "chore: yoを依存関係から削除、npx yo goqooをspawnSyncで実行するように"
    This reverts commit 4399de792257a2daf2c40f4f6e7bf3e7d22cdc84.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
 const { parseArgumentOptions, showVersion, usageExit } = require('./goqoo')
 const path = require('path')
 const fs = require('fs-extra')
-const { spawnSync } = require('child_process')
 
 const argv = parseArgumentOptions()
 if (!argv.subCommand) {
@@ -43,5 +42,8 @@ switch (argv.subCommand) {
     usageExit(1)
   }
 }
-
-spawnSync('npx', ['yo', `goqoo:${subGenerator}`, ...rawArgv], { cwd, stdio: 'inherit' })
+// yo が参照するプロセスの引数とカレントディレクトリを調整
+process.argv = [...process.argv.slice(0, 2), `goqoo:${subGenerator}`, ...rawArgv]
+if (cwd) process.chdir(cwd)
+// ローカルインストールされた yo を呼び出す
+require('yo/lib/cli.js')
