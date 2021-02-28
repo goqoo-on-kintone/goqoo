b12f09e90b1398742c3e5af2c85a0fe636c84664
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-02-28 21:39:55 +0900

    refactor: モジュール分割 yo以外のCLI実行もできるように

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 'use strict'
 
 const { parseArgumentOptions, showVersion, usageExit } = require('./goqoo')
-const path = require('path')
-const fs = require('fs-extra')
+const yo = require('./run-yo')
 
 const argv = parseArgumentOptions()
 if (!argv.subCommand) {
@@ -16,34 +15,8 @@ if (!argv.subCommand) {
   }
 }
 
-const rawArgv = process.argv.slice(3)
-let subGenerator
-let cwd = null
-
 // TODO: --helpの扱いをうまくやる
-switch (argv.subCommand) {
-  case 'init': {
-    subGenerator = 'app'
-    break
-  }
-  case 'new': {
-    subGenerator = 'app'
-    const projectDir = rawArgv.shift()
-    cwd = path.resolve(projectDir)
-    fs.mkdirpSync(cwd)
-    break
-  }
-  case 'generate':
-  case 'g': {
-    subGenerator = `g-${rawArgv.shift()}`
-    break
-  }
-  default: {
-    usageExit(1)
-  }
+
+if (['init', 'new', 'generate', 'g'].includes(argv.subCommand)) {
+  yo(argv)
 }
-// yo が参照するプロセスの引数とカレントディレクトリを調整
-process.argv = [...process.argv.slice(0, 2), `goqoo:${subGenerator}`, ...rawArgv]
-if (cwd) process.chdir(cwd)
-// ローカルインストールされた yo を呼び出す
-require('yo/lib/cli.js')
---
+++ src/run-yo.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const path = require('path')
+const fs = require('fs-extra')
+const { usageExit } = require('./goqoo')
+
+module.exports = (argv) => {
+  const rawArgv = process.argv.slice(3)
+  let subGenerator
+  let cwd = null
+
+  switch (argv.subCommand) {
+    case 'init': {
+      subGenerator = 'app'
+      break
+    }
+    case 'new': {
+      subGenerator = 'app'
+      const projectDir = rawArgv.shift()
+      cwd = path.resolve(projectDir)
+      fs.mkdirpSync(cwd)
+      break
+    }
+    case 'generate':
+    case 'g': {
+      subGenerator = `g-${rawArgv.shift()}`
+      break
+    }
+    default: {
+      usageExit(1)
+    }
+  }
+
+  // yo が参照するプロセスの引数とカレントディレクトリを調整
+  process.argv = [...process.argv.slice(0, 2), `goqoo:${subGenerator}`, ...rawArgv]
+  if (cwd) process.chdir(cwd)
+
+  // ローカルインストールされた yo を呼び出す
+  require('yo/lib/cli.js')
+}
