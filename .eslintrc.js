e5a3d6294d40108bf3a0e5a2df4d2f57613a463d
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-02-28 21:04:49 +0900

    chore: ESLint設定 .jsに変更

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'standard',
+  plugins: ['node', 'mocha'],
+  env: { mocha: true },
+  globals: {
+    // テスト用のグローバル変数 TODO: 自動的にglobalを認識させたい
+    assert: true,
+    goqoo: true,
+  },
+  rules: {
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-debugger': 'off',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'node/no-missing-require': 'error',
+  },
+}
