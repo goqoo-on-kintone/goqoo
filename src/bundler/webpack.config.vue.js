d3169998133c08874e29021829a797937faf259a
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-09 14:37:56 +0900

    refactor: Vueのconfigにwebpack-mergeを利用

--- src/bundler/webpack.config.vue.js
+++ src/bundler/webpack.config.vue.js
@@ -1,52 +1,18 @@
-/* eslint-disable no-console */
-const path = require('path')
-const fs = require('fs')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-const S3Plugin = require('webpack-s3-plugin')
-const merge = require('webpack-merge')
-const { projectPath } = require('../util')
-const { babelOptions, babelOptionsTs } = require('./babel-options')
-require('dotenv').config()
+const config = require('./webpack.config')
+const { mergeWithRules } = require('webpack-merge')
 
-const basePath = path.resolve('src')
-const appsPath = path.join(basePath, 'apps')
-const entry = fs
-  .readdirSync(appsPath)
-  .filter((file) => !/^\./.test(file)) // Exclude dotfiles
-  .reduce(
-    (prev, file) => ({
-      ...prev,
-      [path.parse(file).name]: path.resolve(appsPath, file),
-    }),
-    {}
-  )
+const merge = mergeWithRules({
+  module: {
+    rules: {
+      test: 'match',
+      use: 'replace',
+    },
+  },
+})
 
-const config = {
-  entry,
-  output: { path: path.resolve('dist') },
-  devtool: 'source-map',
+module.exports = merge(config, {
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        loader: require.resolve('babel-loader'),
-        exclude: /node_modules/,
-        options: babelOptions,
-      },
-      {
-        test: /\.ts$/,
-        loader: require.resolve('babel-loader'),
-        exclude: /node_modules/,
-        options: babelOptionsTs,
-      },
-      {
-        test: /\.html$/,
-        loader: require.resolve('html-loader'),
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: require.resolve('style-loader') }, { loader: require.resolve('css-loader') }],
-      },
       {
         test: /\.(scss)$/,
         use: [
@@ -67,66 +33,14 @@ const config = {
         ],
       },
       {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        loader: require.resolve('url-loader'),
+        test: /\.html$/,
+        loader: require.resolve('html-loader'),
       },
     ],
   },
   resolve: {
-    modules: [basePath, 'node_modules'],
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
       vue$: 'vue/dist/vue.esm.js',
     },
   },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        configFile: projectPath('tsconfig.json'),
-      },
-    }),
-  ],
-  devServer: {
-    contentBase: path.resolve('dist'),
-    inline: true,
-    https: true,
-    port: 59000,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    disableHostCheck: true,
-    progress: true,
-  },
-}
-
-if (process.env.S3) {
-  ;['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_S3_REGION', 'AWS_S3_BUCKET'].forEach((variable) => {
-    if (!process.env[variable]) {
-      console.error(`${variable}: environment variable not found!`)
-      process.exit(1)
-    }
-  })
-
-  module.exports = merge(config, {
-    entry: Object.entries(config.entry).reduce((obj, [key, value]) => {
-      obj[`${key}-${process.env.AWS_RANDOM_SUFFIX}`] = value
-      return obj
-    }, {}),
-    plugins: [
-      new S3Plugin({
-        // Exclude uploading of html
-        exclude: /.*\.html$/,
-        // s3Options are required
-        s3Options: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-          region: process.env.AWS_S3_REGION,
-        },
-        s3UploadOptions: {
-          Bucket: process.env.AWS_S3_BUCKET,
-        },
-        basePath: process.env.AWS_S3_BASEPATH || process.env.npm_package_name,
-      }),
-    ],
-  })
-} else {
-  module.exports = config
-}
+})
