6d3c0d72c8b30b91cd469384ce709fcdfc40586b
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-09 15:04:08 +0900

    feat: React bootstrap対応

--- src/bundler/webpack.config.react.js
+++ src/bundler/webpack.config.react.js
@@ -1,10 +1,19 @@
 const config = require('./webpack.config')
-const { merge } = require('webpack-merge')
+const { mergeWithRules } = require('webpack-merge')
 const { babelOptions, babelOptionsTs } = require('./babel-options')
 
 const presetReact = require.resolve('@babel/preset-react')
 babelOptions.presets.push(presetReact)
 babelOptionsTs.presets.push(presetReact)
+
+const merge = mergeWithRules({
+  module: {
+    rules: {
+      test: 'match',
+      use: 'replace',
+    },
+  },
+})
 
 module.exports = merge(config, {
   module: {
@@ -20,6 +29,25 @@ module.exports = merge(config, {
         loader: require.resolve('babel-loader'),
         exclude: /node_modules/,
         options: babelOptionsTs,
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          { loader: require.resolve('style-loader') },
+          { loader: require.resolve('css-loader') },
+          {
+            // for Bootstrap 5.0
+            loader: require.resolve('postcss-loader'),
+            options: {
+              postcssOptions: {
+                plugins: function () {
+                  return [require.resolve('autoprefixer')]
+                },
+              },
+            },
+          },
+          { loader: require.resolve('sass-loader') },
+        ],
       },
     ],
   },
