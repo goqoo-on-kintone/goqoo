f6e0b64f4c4dc7b6cd98ecd34253417a4705bae9
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-12 17:59:39 +0900

    chore: dts-genのラッパーを実装

---
+++ src/generator/dts/index.ts
@@ -0,0 +1,50 @@
+import { spawn } from 'child_process'
+import { mkdirSync } from 'fs'
+import { paramCase as kebabCase, pascalCase } from 'change-case'
+// @ts-ignore
+import { projectPath } from '../../util'
+import type { ConfigBase } from '../../types/goqoo.types'
+
+export const dts = (config: ConfigBase) => {
+  const { dtsGen } = config
+  const context = config.environments.find((c) => c.env === dtsGen?.env)
+  if (!context) {
+    throw new Error('dts-gen context not found!')
+  }
+
+  const dist = 'src/types'
+  mkdirSync(dist, { recursive: true })
+
+  const connection = {
+    'base-url': `https://${context.domain}`,
+    'username': process.env.GOQOO_USERNAME,
+    'password': process.env.GOQOO_PASSWORD,
+    'basic-auth-username': process.env.GOQOO_BASICAUTH_USERNAME,
+    'basic-auth-password': process.env.GOQOO_BASICAUTH_PASSWORD,
+  }
+  Object.entries(context.appId).forEach(([appName, appId]) => {
+    const args = {
+      ...connection,
+      'type-name': `${pascalCase(appName)}Fields`,
+      'app-id': appId,
+      'output': `${dist}/${kebabCase(appName)}-fields.d.ts`,
+    }
+
+    const process = spawn(
+      'npx',
+      ['kintone-dts-gen', ...Object.entries(args).map(([key, value]) => `--${key}=${value}`)],
+      {
+        cwd: projectPath('./'),
+        stdio: 'inherit',
+      }
+    )
+
+    // 終了時の処理
+    process.on('close', (code) => {
+      if (code !== 0) {
+        console.error(`kintone-dts-gen process exited with code ${code}`)
+      }
+      console.log(`dts-gen: ${args.output}`)
+    })
+  })
+}
---
+++ src/types/goqoo.types.ts
@@ -0,0 +1,31 @@
+// TODO: goqoo本体に持っていく
+
+export type AppId = Record<string, number>
+export type ApiToken = Record<string, string>
+
+export interface ContextBase {
+  env: string
+  domain: string
+  appId: AppId
+  apiToken?: ApiToken
+  guest?: number
+}
+
+export interface Context<TEnv extends string, TAppId extends AppId, TApiToken extends ApiToken> extends ContextBase {
+  env: TEnv
+  domain: string
+  appId: TAppId
+  apiToken?: TApiToken
+  guest?: number
+}
+
+export interface ConfigBase {
+  bundlerType?: 'standard' | 'vue' | 'react'
+  dtsGen?: { env: string; skip?: string }
+  environments: ContextBase[]
+}
+
+export interface Config<TEnv extends string, TAppId extends AppId, TApiToken extends ApiToken> extends ConfigBase {
+  dtsGen?: { env: TEnv; skip?: string }
+  environments: Context<TEnv, TAppId, TApiToken>[]
+}
