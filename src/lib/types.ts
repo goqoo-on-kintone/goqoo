843ca74860c753e450494b2bd3d018014b079f86
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-19 16:31:22 +0900

    feat: contextにviewIdの定義を追加

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,14 +1,17 @@
 export type AppId = Record<string, number>
 export type ApiToken = Record<string, string>
+export type ViewId = Partial<Record<keyof AppId, Record<string, number>>>
 
 export type Context<
   TEnv extends string = string,
   TAppId extends AppId = AppId,
+  TViewId extends ViewId = ViewId,
   TApiToken extends ApiToken = ApiToken
 > = {
   env: TEnv
   domain: string
   appId: TAppId
+  viewId?: TViewId
   apiToken?: TApiToken
   guest?: number
 }
@@ -16,9 +19,10 @@ export type Context<
 export type Config<
   TEnv extends string = string,
   TAppId extends AppId = AppId,
+  TViewId extends ViewId = ViewId,
   TApiToken extends ApiToken = ApiToken
 > = {
   bundlerType?: 'default' | 'react' | 'vue'
   dtsGen?: { env: TEnv; skip?: string[] }
-  environments: Context<TEnv, TAppId, TApiToken>[]
+  environments: Context<TEnv, TAppId, TViewId, TApiToken>[]
 }
