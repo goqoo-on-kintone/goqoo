41de75bd9afda651ce77ab3bf4eff4dd60722b65
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-12-21 00:40:45 +0900

    feat: TODOコメント追加 getSpaceId()について

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -4,3 +4,10 @@ export const getQueryOrder = (): string => {
   const orderBy = Array.isArray(matchResult) ? matchResult[1] : ''
   return orderBy
 }
+
+// TODO: getSpaceId()関数を追加
+// async関数になる
+// * URLにスペースIDが含まれていればそれを取得
+// * kintone.app.getId()が値を返せば、そこからREST API経由で取得
+// * ゲストスペースかどうかも分かる
+// * ポータルで叩いた場合はundefinedかエラーか
